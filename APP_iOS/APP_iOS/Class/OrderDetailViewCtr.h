d95903a6b10858bc960b3622c5c7e98e55318457
linxiaoguo <jmulxg@qq.com>
2015-11-15 13:01:02 +0800

    订单管理

---
+++ APP_iOS/APP_iOS/Class/OrderDetailViewCtr.h
@@ -0,0 +1,13 @@
+//
+//  OrderDetailViewCtr.h
+//  APP_iOS
+//
+//  Created by 林小果 on 15/11/14.
+//  Copyright © 2015年 Li. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface OrderDetailViewCtr : BaseViewController
+
+@end
---
+++ APP_iOS/APP_iOS/View/OrderTitleCell.h
@@ -0,0 +1,16 @@
+//
+//  OrderTitleCell.h
+//  APP_iOS
+//
+//  Created by 林小果 on 15/11/14.
+//  Copyright © 2015年 Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrderTitleCell : UITableViewCell
+
+
+@property (nonatomic, strong)IBOutlet UIImageView *imvIcon;
+@property (nonatomic, strong)IBOutlet UILabel *lblTitle;
+@end
