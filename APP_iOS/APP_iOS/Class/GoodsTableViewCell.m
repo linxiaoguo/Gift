dcccba5e822423d6d0ada950f1db1e8c0564faca
linxiaoguo <jmulxg@qq.com>
2015-11-18 09:20:25 +0800

    修改名称

--- APP_iOS/APP_iOS/Class/GoodsTableViewCell.m
+++ APP_iOS/APP_iOS/Class/GoodsTableViewCell.m
@@ -25,8 +25,8 @@
     [_headImage sd_setImageWithURL:[NSURL URLWithString:goodModel.pic] placeholderImage:[UIImage imageNamed:@"cpxqq"]];
     _nameLabel.text = goodModel.name;
     _desLabel.text = goodModel.name;
-    _priceLabel.text = [NSString stringWithFormat:@"￥%@", goodModel.price];
-    _saleLabel.text = [NSString stringWithFormat:@"销量 %d    库存 %d", goodModel.sales, goodModel.stock];
+    _priceLabel.text = [NSString stringWithFormat:@"￥%f", goodModel.price];
+    _saleLabel.text = [NSString stringWithFormat:@"销量 %ld    库存 %ld", (long)goodModel.sales, (long)goodModel.stock];
     _timeLabel.text = [NSString stringWithFormat:@"添加时间 %@", [ShareFunction stringWithTimestamp:goodModel.addtime]];
 
 }
