5720703edcd286088481384dc8a75c4f50cf9df0
linxiaoguo <jmulxg@qq.com>
2016-01-13 09:49:22 +0800

    暂停DLog引起的卡顿

--- APP_iOS/APP_iOS/Request/LHttpRequest/LHttpConfig.h
+++ APP_iOS/APP_iOS/Request/LHttpRequest/LHttpConfig.h
@@ -10,10 +10,3 @@
 #define kSuccessCode    @"1"
 #define kSuccessKey     @"success"
 
-#ifndef DLog
-#ifdef DEBUG
-#define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
-#else
-#define DLog(...)
-#endif
-#endif
